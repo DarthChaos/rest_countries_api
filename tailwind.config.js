946f11632e4474240e967531989119e4502464d0
DarthChaos <63266271+DarthChaos@users.noreply.github.com>
2022-02-15 11:10:27 -0500

    fixed redux and some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    screens: {
+      mobile: '375px',
+      desktop: '1440px',
+    },
+    extend: {
+      colors: {
+        'dark-blue': 'hsl(209, 23%, 22%)',
+        'very-dark-blue': 'hsl(207, 26%, 17%)',
+        'almost-black-blue': 'hsl(200, 15%, 8%)',
+        'dark-gray': 'hsl(0, 0%, 52%)',
+        'very-light-gray': 'hsl(0, 0%, 98%)',
+      },
+    },
   },
   plugins: [],
 };
